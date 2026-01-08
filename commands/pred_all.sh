511df65a9419b0850746333a07eab046e5fdd7d3
hyunjincho <hyunjincho84@gmail.com>
2026-01-08 05:41:53 +0000

    evaluation

--- commands/pred_all.sh
+++ commands/pred_all.sh
@@ -47,17 +47,7 @@ section "[2/2] GPT-5.1 Evaluation"
 step "Sampling examples for GPT-5.1"
 python3 ../model_tests/get_samples_for_gpt.py
 
-step "[1/2] BERT prediction on GPT samples"
-python3 pred_bert_with_gpt_samples.py \
-  --in-root /home/jovyan/LEM_data2/hyunjincho/gpt_samples \
-  --out-root /home/jovyan/LEM_data2/hyunjincho/bert_pred_new/gpt_sample \
-  --model-name /home/jovyan/LEM_data2/hyunjincho/bert_pretrained/checkpoint-165687 \
-  --best-model-pt "/home/jovyan/LEM_data2/hyunjincho/checkpoints(www)_new/best_model.pt" \
-  --vocab-path /home/jovyan/LEM_data2/hyunjincho/preprocessed_www_new/skill2idx.json \
-  --topk 5 \
-  --batch-size 64
-
-step "[2/2] GPT-5.1 prediction"
+step "[1/1] GPT-5.1 prediction"
 python3 ../model_tests/test_gpt.py
 
 section "PIPELINE COMPLETED SUCCESSFULLY"
